288101e51ac53cdc344058c2d15e948f6707b459
Kurt <Kurt@MSI>
2023-05-29 22:41:01 +0800

    Add project files.

---
+++ dataupload/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.UploadButton_Click = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(313, 31)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(793, 379)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'UploadButton_Click
+        '
+        Me.UploadButton_Click.BackColor = System.Drawing.SystemColors.Info
+        Me.UploadButton_Click.Location = New System.Drawing.Point(76, 169)
+        Me.UploadButton_Click.Name = "UploadButton_Click"
+        Me.UploadButton_Click.Size = New System.Drawing.Size(188, 72)
+        Me.UploadButton_Click.TabIndex = 1
+        Me.UploadButton_Click.Text = "Upload"
+        Me.UploadButton_Click.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1160, 450)
+        Me.Controls.Add(Me.UploadButton_Click)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents UploadButton_Click As Button
+End Class
---
+++ dataupload/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Data
+Imports System.IO
+Imports Microsoft.VisualBasic.FileIO
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles UploadButton_Click.Click
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "CSV files (*.csv)|*.csv"
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim filePath As String = openFileDialog.FileName
+
+            ' Load data from CSV file
+            Dim dataTable As DataTable = LoadDataFromCSV(filePath)
+
+            ' Display the data in the DataGridView
+            DataGridView1.DataSource = dataTable
+        End If
+    End Sub
+    Private Function LoadDataFromCSV(filePath As String) As DataTable
+        Dim dataTable As New DataTable()
+
+        Using parser As New TextFieldParser(filePath)
+            parser.TextFieldType = FieldType.Delimited
+            parser.SetDelimiters(",")
+
+            ' Read the header row
+            Dim headers As String() = parser.ReadFields()
+            For Each header In headers
+                dataTable.Columns.Add(header)
+            Next
+
+            ' Read the data rows
+            While Not parser.EndOfData
+                Dim fields As String() = parser.ReadFields()
+                dataTable.Rows.Add(fields)
+            End While
+        End Using
+
+        Return dataTable
+    End Function
+End Class
